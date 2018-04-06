13582a8709b97f3ed2a85359f1c70d1dab10fe20
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-04-06 09:27:02 +0100

    add random sensor tests

--- test/ghosts-can-be-detected.spec.js
+++ test/ghosts-can-be-detected.spec.js
@@ -1,18 +1,21 @@
-const ghostDetector = require('../src/ghostDetector.js')
 const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
 
+const ghostDetector = require('../src/ghostDetector.js')
+
+const alwaysDetectThree = {
+  detect: () => (Promise.resolve(3))
+}
+
 describe('the ghost detector', function () {
   it('generates one spookiness unit for each "hour" it is running', function (done) {
     ghostDetector
-      .withSensor({
-        detect: () => {
-          return Promise.resolve(3)
-        }})
+      .withSensor(alwaysDetectThree)
       .runForHours(7)
       .then(spookyReadings => {
         expect(spookyReadings.length).to.eql(7)
+        spookyReadings.every(sr => expect(sr).to.eql(3))
         done()
       })
       .catch(done)
---
+++ test/random-sensor-spec.js
@@ -0,0 +1,20 @@
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+
+const randomSensor = require('../src/randomSensor')
+
+describe('the random sensor', function () {
+  it('generates millispooks between zero and ten', function (done) {
+    Promise.all(Array(100).fill()
+      .map(randomSensor.detect)
+    )
+      .then(xs => {
+        xs.every(x => {
+          expect(x).to.be.within(0, 10)
+        })
+        done()
+      })
+      .catch(done)
+  })
+})
