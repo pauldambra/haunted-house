855054beec5ec59f516f9f6d572e3700553bf51e
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-04-06 09:35:10 +0100

    move ghost detector setup out of test

--- test/ghosts-can-be-detected.spec.js
+++ test/ghosts-can-be-detected.spec.js
@@ -2,16 +2,15 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
 
-const ghostDetector = require('../src/ghostDetector.js')
-
 const alwaysDetectThree = {
   detect: () => (Promise.resolve(3))
 }
 
+const ghostDetector = require('../src/ghostDetector.js').withSensor(alwaysDetectThree)
+
 describe('the ghost detector', function () {
   it('generates one spookiness unit for each "hour" it is running', function (done) {
     ghostDetector
-      .withSensor(alwaysDetectThree)
       .runForHours(7)
       .then(spookyReadings => {
         expect(spookyReadings.length).to.eql(7)
