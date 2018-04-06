ca753955473f60dbdd3e0e64fbcf9ab7a8e58e58
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-04-06 09:26:54 +0100

    Add wallabyjs conf

---
+++ wallaby.conf.js
@@ -0,0 +1,14 @@
+module.exports = function (wallaby) {
+  return {
+    files: [
+      'src/**/*.js'
+    ],
+    tests: [
+      'test/**/*-spec.js'
+    ],
+    env: {
+      type: 'node'
+    },
+    debug: true
+  }
+}
